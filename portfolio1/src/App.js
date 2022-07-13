3e8b267b17034a7c75a078b2706c8dc657ba0352
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-13 16:45:56 +0545

    Adding files

--- portfolio1/src/App.js
+++ portfolio1/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./files/nabbar";
+import { Banner } from "./files/banner";
+import { Skills } from "./files/skills";
+import { Projects } from "./files/project";
+import { Contact } from "./files/contact";
+import { Footer } from "./files/footer";
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
