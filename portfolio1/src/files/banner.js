43f9128834c84461a3aae65fdda1e73c683f087b
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-15 12:07:45 +0545

    Update banner.js

--- portfolio1/src/files/banner.js
+++ portfolio1/src/files/banner.js
@@ -1,55 +1,79 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
+
+// animation of circle arrow moving to right while hovering
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
+
+  // numbers of letter dispalyed in the screen
   const [loopNum, setLoopNum] = useState(0);
+
+  // deleting the current letters in the screen
   const [isDeleting, setIsDeleting] = useState(false);
+
+  // storing the current text we are displaying
   const [text, setText] = useState('');
+
+  // storing how fast the word is being deleted or typed
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
+
+  // words that will be displayed on the screen
   const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
   const period = 2000;
 
+  // useEffect to type or delete the current word
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, delta);
 
+    // clearing the interval
     return () => { clearInterval(ticker) };
-  }, [text])
+
+    // eslint-disable-next-line
+  }, [text])     // it is used every time the text is updated
 
   const tick = () => {
+    // to only store the value upto the index number of 2
     let i = loopNum % toRotate.length;
+
+    // storing the current situation of the text that is displayed
     let fullText = toRotate[i];
+
+    // if the state is deleting it will take text.length - 1 else it will take text.length + 1
     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
+    // chaning the pace while deleting the word
     if (isDeleting) {
       setDelta(prevDelta => prevDelta / 2);
     }
 
+    // if the text is full displayed then changing the pace and chaning setIsDeleting to true
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === '') { // when the text is fully deleted
       setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
+      setLoopNum(loopNum + 1); // chaning the word
       setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
+    } 
   }
 
   return (
+
+    // body content of the portfolio
     <section className="banner" id="home">
       <Container>
+
+        {/* setting the items in center and display in row format */}
         <Row className="aligh-items-center">
+
+          {/* setting the size of the left box */}
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
@@ -57,14 +81,20 @@ export const Banner = () => {
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button> 
               </div>}
             </TrackVisibility>
           </Col>
+
+          {/* setting the size of the right box */}
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
               {({ isVisible }) =>
+
+                // animation of the header image
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+
+                  {/* header img displayed from assets */}
                   <img src={headerImg} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
