7403916febccdf0119646ca4d4e1db4e174f119b
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-26 22:01:14 +0545

    Update footer.js

--- portfolio1/src/files/footer.js
+++ portfolio1/src/files/footer.js
@@ -1,5 +1,6 @@
+// importing bootstrap
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
+
 import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
@@ -10,7 +11,6 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
