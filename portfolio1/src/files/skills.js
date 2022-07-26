1538ea457b35c045cf52944c3652b572b6cbabf6
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-26 22:01:18 +0545

    Update skills.js

--- portfolio1/src/files/skills.js
+++ portfolio1/src/files/skills.js
@@ -3,8 +3,6 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-// import arrow1 from "../assets/img/arrow1.svg";
-// import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -42,16 +40,12 @@ export const Skills = () => {
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Ratio" />
-                                <h5>UI/UX Design</h5>
+                                <img src={meter2} alt="Ratio" /> 
+                                <h5>UI/UX Design</h5>                               
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Ratio" />
                                 <h5>Game Developer</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Ratio" />
-                                <h5>Web Development</h5>
                             </div>
                         </Carousel>
                     </div>
