ed12b1f2ce264eb58cc1e912e64c98b3088a5bcf
Aayam <aayamkhatiwada123@gmail.com>
2023-01-27 19:23:49 +0545

    Sovled the problems on project link of navbar

--- portfolio1/src/files/banner.js
+++ portfolio1/src/files/banner.js
@@ -2,106 +2,75 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 
-// animation of circle arrow moving to right while hovering
 import { ArrowRightCircle } from 'react-bootstrap-icons';
-import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
 
-  // numbers of letter dispalyed in the screen
   const [loopNum, setLoopNum] = useState(0);
-
-  // deleting the current letters in the screen
   const [isDeleting, setIsDeleting] = useState(false);
-
-  // storing the current text we are displaying
   const [text, setText] = useState('');
-
-  // storing how fast the word is being deleted or typed
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-
-  // words that will be displayed on the screen
+  const [delta, setDelta] = useState(500 - Math.random() * 100);
   const toRotate = ["Web Developer", "Web Designer", "Mobile App developer"];
   const period = 2000;
 
-  // useEffect to type or delete the current word
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, delta);
 
-    // clearing the interval
     return () => { clearInterval(ticker) };
 
-    // eslint-disable-next-line
-  }, [text])     // it is used every time the text is updated
+  }, [text])
 
   const tick = () => {
-    // to only store the value upto the index number of 2
     let i = loopNum % toRotate.length;
-
-    // storing the current situation of the text that is displayed
     let fullText = toRotate[i];
-
-    // if the state is deleting it will take text.length - 1 else it will take text.length + 1
     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
     setText(updatedText);
 
-    // chaning the pace while deleting the word
     if (isDeleting) {
       setDelta(prevDelta => prevDelta / 2);
     }
 
-    // if the text is full displayed then changing the pace and chaning setIsDeleting to true
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') { // when the text is fully deleted
+    } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
-      setLoopNum(loopNum + 1); // chaning the word
-      setDelta(500);
+      setLoopNum(loopNum + 1);
+      setDelta(500 - Math.random() * 100);
     }
+  }
+
+  const changeToConnect = () => {
+    let newLink = '';
+    if (window.location.href.includes("#")) {
+      newLink = window.location.href.slice(0, window.location.href.indexOf("#"))
+    }
+    window.location.href = newLink + "#connect"
   }
 
   return (
     <>
-      {/* // body content of the portfolio */}
       <section className="banner" id="home">
         <Container>
 
-          {/* setting the items in center and display in row format */}
           <Row className="aligh-items-center">
-
-            {/* setting the size of the left box */}
             <Col xs={12} md={6} xl={7}>
-              <TrackVisibility>
-                {({ isVisible }) =>
-                  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                    <span className="tagline">Welcome to my Portfolio</span>
-                    <h1>{`Hi! It's me Aayam Khatiwada`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "Mobile App developer" ]'><span className="wrap">{text}</span></span></h1>
-                    <p>An undergraduate, with a desire and dedication to learn new skills and improve it. Have a some
-                      understanding on programming languages and also have basic understanding on platform. Self learner with the a strong passion on new and creative working and want to get experience
-                      through mentorship.</p>
-                    <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
-                  </div>}
-              </TrackVisibility>
+              <span className="tagline">Welcome to my Portfolio</span>
+              <h1>{`Hi! It's me Aayam Khatiwada`} <span className="txt-rotate"><span className="wrap">{text}</span></span></h1>
+              <p>An undergraduate, with a desire and dedication to learn new skills and improve it. Have a some
+                understanding on programming languages and also have basic understanding on platform. Self learner with the a strong passion on new and creative working and want to get experience
+                through mentorship.</p>
+              <button onClick={() => changeToConnect()}>Let's Connect <ArrowRightCircle size={25} /></button>
+
             </Col>
 
-            {/* setting the size of the right box */}
             <Col xs={12} md={6} xl={5}>
-              <TrackVisibility>
-                {({ isVisible }) =>
-
-                  // animation of the header image
-                  <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-
-                    {/* header img displayed from assets */}
-                    <img src={headerImg} alt="Header Img" />
-                  </div>}
-              </TrackVisibility>
+              <img src={headerImg} alt="Header Img" />
             </Col>
           </Row>
+
         </Container>
       </section>
     </>
--- portfolio1/src/files/nabbar.js
+++ portfolio1/src/files/nabbar.js
@@ -1,8 +1,6 @@
-// importing libraries
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
-// importing images
 import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
@@ -10,17 +8,12 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
 
-  // useState to store which link we are currently on
   const [activeLink, setActiveLink] = useState('home');
-
-  // useState to store background color when user scroll or not
   const [scrolled, setScrolled] = useState(false);
 
-  // useEffect weather the user scrolls or not
   useEffect(() => {
     const onScroll = () => {
 
-      // check the y-axis to determine the weather the user scrolled or not
       if (window.scrollY > 50) {
         setScrolled(true);
       } else {
@@ -28,52 +21,43 @@ export const NavBar = () => {
       }
     }
 
-    // function works only when the user scrlls
     window.addEventListener("scroll", onScroll);
 
-    // removing the event listener when component is removed from dom
     return () => window.removeEventListener("scroll", onScroll);
   }, [])
 
-  // function to update the current activeLink
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   }
 
   return (
-    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>{/* checking weather it is scrolled or not*/}
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
 
-        {/* displays logo */}
         <Navbar.Brand href="/">
           <img src={logo} alt="Logo" />          
         </Navbar.Brand>
 
-        {/* image when displayed in mobile or small screen */}
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
 
         <Navbar.Collapse id="basic-navbar-nav">
 
-          {/* links on the home skills projects */}
           <Nav className="ms-auto">
 
-            {/* storing which link is currently displayed on dom */}
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
 
-            {/* links for the media icons */}
             <div className="social-icon">
               <a href="https://github.com/AayamKhatiwada"><img src={navIcon1} alt="" /></a>
               <a href="https://github.com/AayamKhatiwada"><img src={navIcon2} alt="" /></a>
               <a href="https://github.com/AayamKhatiwada"><img src={navIcon3} alt="" /></a>
             </div>
 
-            {/* Button 'Let's connect' */}
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
         </Navbar.Collapse>
